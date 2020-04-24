d6cae33258c563adbb6c07b7bd9d13f7083abec2
Eduardo Araújo <eduardoaraujo@MBP-de-Eduardo.lan>
2020-04-24 23:51:11 +0100

    Form fix

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,43 +1,27 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { FormContainer } from '../styles/components/FormContainer'
 import { SoftElement } from '../styles/components/SoftElement'
 import { Button } from '../styles/components/Button'
 
 const ContactForm = () => {
-    const [nameInput, setNameInput] = useState('')
-    const [emailInput, setEmailInput] = useState('')
-    const [textInput, setTextInput] = useState('')
-
-    const submitForm = (e) => {
-        e.preventDefault()
-        console.log(nameInput)
-        console.log(emailInput)
-        console.log(textInput)
-    }
 
     return (
-        <FormContainer onSubmit={e => submitForm(e)} name="contact" method="POST" data-netlify="true">
+        <FormContainer name="contact" method="POST" data-netlify="true">
             <SoftElement className="input-wrapper">
                 <input 
                     type="text" 
                     placeholder="Tell me your name"
-                    value={nameInput}
-                    onChange={e => setNameInput(e.target.value)}
                 />
             </SoftElement>
             <SoftElement className="input-wrapper">
                 <input 
                     type="email" 
                     placeholder="What's your email?"
-                    value={emailInput}
-                    onChange={e => setEmailInput(e.target.value)}
                 />
             </SoftElement>
             <SoftElement className="input-wrapper">
                 <textarea
                     placeholder="Write your message"
-                    value={textInput}
-                    onChange={e => setTextInput(e.target.value)}
                 ></textarea>
             </SoftElement>
             <Button className="bg-hover" big>Send message</Button>
